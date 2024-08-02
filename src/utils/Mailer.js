7dea98941dd22c5ac31883ccbb6de5ac6a9e2ed0
ayush <ayushbhatt204@gmail.com>
2024-08-02 19:10:47 +0530

    testing commit

--- src/utils/Mailer.js
+++ src/utils/Mailer.js
@@ -19,7 +19,7 @@ export const sendEmail = async ({ email, emailType, userId }) => {
             port: 587,
             secure: false, // Use `true` for port 465, `false` for all other ports
             auth: {
-                user: "maddison53@ethereal.email",
+                user: "maddis3@ethereal.email",
                 pass: "jn7jnAPss4f63QBp6D",
             },
         });
